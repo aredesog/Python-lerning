fcc0bdca3e60ab07b129bd9c0a76b57e9c2d0795
Guilherme Aredes <guilherme.aredes@sou.unifal-mg.edu.br>
2025-09-20 22:09:39 -0300

    Adicionar anotações sobre lógica de programação e introduzir conceitos básicos de Python

---
+++ BASICO/pratica.py
@@ -0,0 +1,24 @@
+# Uso dos Exercicos pré ditados na ALURA para aprofundamento do conteúdo
+
+print('\nPython na Escola de Programação da Alura\n')
+
+# Declaração de variáveis e inserção em uma frase 
+nome = 'Guilherme'
+idade = 19 
+print(f'Meu nome é {nome} e tenho {idade} anos')
+
+#Impressão da palavra ALURA
+
+print(
+'''
+A
+L
+U
+R
+A
+''')
+
+# Frase com valor de Pi
+
+pi = 3.14159 
+print(f'O valor arredondado de pi é: {pi:.2f}')
