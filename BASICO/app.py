2f2b10b0e884d331a641a7887462c4096bb290a2
Guilherme Aredes <guilherme.aredes@sou.unifal-mg.edu.br>
2025-09-21 10:50:57 -0300

    Adicionar anotações sobre funções e condicionais em Python

--- BASICO/app.py
+++ BASICO/app.py
@@ -1,16 +1,52 @@
-print('''
-░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
-██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
-╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
-░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
-██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
-╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
-''')
+#Bibliotecas do Python
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurante')
-print('3. Ativar Restaurante')
-print('4. Sair')
+import os
 
-opcao_escolhida = input('\nEscolha sua opção: ')
-print(f'A opção escolhida foi: {opcao_escolhida}') # O uso do (f'x {y}?'), é outro metodo para a vizualização da variavel, = ('x', opcao_escolhida) 
+def exibir_nome():
+    print('''
+    ░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+    ██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+    ╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+    ░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+    ██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+    ╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+    ''')
+
+def exibir_opcoes():
+    print('1. Cadastrar Restaurante')
+    print('2. Listar Restaurante')
+    print('3. Ativar Restaurante')
+    print('4. Sair')
+
+
+
+#Exemplo de função em Python
+
+def finalizar_app():
+    os.system('cls') #Limpa o terminal e exibe apenas o conteudo da função
+    print('Encerrando o App')
+
+#Apendendo if, else e elif no python (CONDICIONAIS)
+
+def escolher_opcao():
+
+    opcao_escolhida = int(input('\nEscolha sua opção: ')) 
+    #opcao_escolhida = int (opcao_escolhida) -> Transforma a opcao_escolhida em uma variavel do tipo int
+    #print(f'A opção escolhida foi: {opcao_escolhida}')  O uso do (f'x {y}?'), é outro metodo para a vizualização da variavel
+    
+    if opcao_escolhida == 1:
+        print('Cadastrar Restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar Restaurante')
+    elif opcao_escolhida == 3:
+        print('Ativar Restaurante')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nome()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+        main()
--- BASICO/pratica.py
+++ BASICO/pratica.py
@@ -1,3 +1,5 @@
+'''
+
 # Uso dos Exercicos pré ditados na ALURA para aprofundamento do conteúdo
 
 print('\nPython na Escola de Programação da Alura\n')
@@ -11,14 +13,73 @@ print(f'Meu nome é {nome} e tenho {idade} anos')
 
 print(
 '''
-A
-L
-U
-R
-A
+#A
+#L
+#U
+#R
+#A
 ''')
 
 # Frase com valor de Pi
 
 pi = 3.14159 
 print(f'O valor arredondado de pi é: {pi:.2f}')
+
+'''
+
+#Solicite ao usuário que insira um número, par ou impara?
+
+print('\n IMPAR ou PAR?\n')
+
+numero = int(input('Digite um número\n'))
+
+if numero % 2 == 0: 
+    print(f'O numero {numero} é par')
+else:
+    print(f'O numero {numero} é impar')
+
+#Pergunte da idade e clçassifique
+
+print('\nVerficação de idade\n')
+
+idade = int(input('Digite a sua idade:'))
+
+if idade <= 12:
+    print(f'Criança pela idade {idade}')
+elif 13<= idade <= 18:
+    print(f'Adolescente pela idade {idade}')
+else:
+    print(f'Adulto pela idade {idade}')
+
+#Solicite uma senha e um nome de Usuario e faça a a verificação
+
+print('\nVerificação de Usuário\n')
+
+nome = str(input('Digite seu nome de Usuário: '))
+senha = int(input('Digite sua senha: '))
+
+nome_verificado = str(input('\nDigite seu nome de Usuário: '))
+senha_verficada = int(input('Digite sua senha: '))
+
+if nome == nome_verificado and senha == senha_verficada:
+    print('\nAcesso Liberado')
+else:
+    print('\nAcesso Negado')
+    
+#Verificação de coordenadas e quadrantes
+
+print('\nCoordenadas e seus Quadrantes\n')
+
+x = int(input('Digite as coordenadas de x:'))
+y = int(input('Digite as coordenadas de y:'))
+
+if x > 0 and y > 0:
+    print('Coordenada localizada no 1° Quadrante')
+elif x < 0 and y > 0:
+    print('Coordenada localizada no 2° Quadrante')
+elif x > 0 and y < 0:
+    print('Coordenada localizada no 3° Quadrante')
+elif x < 0 and y < 0:
+    print('Coordenada localizada no 4° Quadrante')
+else:
+    print('Ponto localizado na origem!')
