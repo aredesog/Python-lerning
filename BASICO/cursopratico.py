6c286552e948a313ed10b5a2806aa7648e752b25
Guilherme <guilherme.aredes@sou.unifal-mg.edu.br>
2025-11-11 15:26:55 -0300

    Adicionar cálculos de IMC, controle de orçamento, controle de acesso, classificação de estudantes e cálculo de pedágio no arquivo cursopratico.py

--- BASICO/cursopratico.py
+++ BASICO/cursopratico.py
@@ -32,6 +32,60 @@ else:
     print(f'Temperatura = a {temperatura}°')
 '''
 #Calculo de IMC
-
+'''
 peso = int(input('DIgite seu peso: '))
-altura = int(input('Digite sua altura'))
+altura = float(input('Digite sua altura:'))
+
+IMC = peso/(altura*altura)
+
+if IMC < 18.5:
+    print('abaixo do peso')
+elif IMC >= 18.5 and IMC < 25:
+    print('peso normal')
+else:
+    print('acima do peso')
+'''
+#Controlando orçamento mensal
+'''
+limite = 3000.0
+despesas = float(input("Digite o total de despesas do mês (R$): "))
+
+if despesas > limite:
+    print("Atenção! Você ultrapassou o limite do orçamento.")
+else:
+    print("Você está dentro do orçamento.")
+'''
+#COntrole de acesso ao escritorio
+hora_atual = int(input("Digite a hora atual (formato 24 horas): "))
+'''
+if 8 <= hora_atual < 18:
+    print("Acesso permitido.")
+else:
+    print("Acesso negado.")
+'''
+#Classificando estudantes 
+'''
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+if media >= 7:
+    print("Aprovado")
+elif 5 <= media < 7:
+    print("Recuperação")
+else:
+    print("Reprovado")
+'''
+#Calculando Pedagio
+'''
+distancia = float(input("Digite a distância percorrida (em km): "))
+
+if distancia <= 100:
+    print("Valor do pedágio: R$ 10,00")
+elif 100 < distancia <= 200:
+    print("Valor do pedágio: R$ 20,00")
+else:
+    print("Valor do pedágio: R$ 30,00")
+'''
