2f630357a933b5c4b0262528f19cc71601ac1069
Guilherme <guilherme.aredes@sou.unifal-mg.edu.br>
2025-11-11 13:52:29 -0300

    Adicionar estrutura de controle para cálculo de temperatura e IMC no arquivo cursopratico.py

--- BASICO/cursopratico.py
+++ BASICO/cursopratico.py
@@ -11,7 +11,7 @@ elif x > y:
     print(f'\nAs mais vendidas foram as bananas: {x}')
 '''
 #Cauculando o tempo total do projeto
-
+'''
 A = input('Digite o tempo do projeto A: ')
 B = input('Digite o tempo do projeto B: ')
 C = input('Digite o tempo do projeto C: ')
@@ -21,3 +21,17 @@ if (A >= 0 and B >= 0 and C >= 0):
     print(f"O tempo total do projeto é de {tempo_total} dias.")
 else: 
     print("Erro: Os dias não podem ser negativos.")
+'''
+#Temperatura dos servidores
+'''
+temperatura = int(input('Digite e atemperatura:'))
+
+if temperatura >= 26:
+    print('Alerta de perigo, temperatura muito alta!')
+else:
+    print(f'Temperatura = a {temperatura}°')
+'''
+#Calculo de IMC
+
+peso = int(input('DIgite seu peso: '))
+altura = int(input('Digite sua altura'))
